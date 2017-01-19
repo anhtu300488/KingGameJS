4d7102d18be799452d29e895fe1a3843692932b2
anhtu300488 <buianhtu3004@gmail.com>
2017-01-19 10:38:32 +0700

    change picture

--- ClientJs/src/Scenes/PlayScene.js
+++ ClientJs/src/Scenes/PlayScene.js
@@ -11,7 +11,7 @@ var PlayLayer = cc.Layer.extend({
     init:function () {
         this._super();
 
-        var spriteBG = new cc.Sprite(res.COMMON_SPRITE_ITEM_BACKGROUND);
+        var spriteBG = new cc.Sprite(res.COMMON.SPRITE_ITEM_BACKGROUND);
 
         var spriteWidth = spriteBG.getContentSize().width;
 
@@ -21,7 +21,7 @@ var PlayLayer = cc.Layer.extend({
         var cols = visibleSize.height/ spriteHeight + 1;
         for(var i = 0; i< rows; i++){
             for(var j = 0; j<cols; j++){
-                var itemSpriteBG = new cc.Sprite(res.COMMON_SPRITE_ITEM_BACKGROUND);
+                var itemSpriteBG = new cc.Sprite(res.COMMON.SPRITE_ITEM_BACKGROUND);
                 var centerPos = cc.p(spriteBG.x + i*spriteWidth, spriteBG.y + j*spriteHeight);
                 itemSpriteBG.setPosition(centerPos);
                 this.addChild(itemSpriteBG);
